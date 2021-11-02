f4ffbb13cd9e90e02f55f6ab7b7ca4e3f2086526
Haihan <x1442565260@gmail.com>
2021-11-02 22:00:17 +0100

    delete old file

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module WidocoGui {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires transitive javafx.graphics;
-	
-	opens application to javafx.graphics, javafx.fxml;
-	
-	exports application;
-}
